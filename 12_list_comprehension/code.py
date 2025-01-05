1bb20649b0e2476749e70ff3357b5ba9763bec45
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-05 16:33:22 -0300

    12_list_comprehension

---
+++ 12_list_comprehension/code.py
@@ -0,0 +1,8 @@
+numbers = [1, 3, 5]
+doubled = [x * 2 for x in numbers]
+print(doubled)
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+starts_s = [f for f in friends if f.startswith("S")]
+print(starts_s)
+
