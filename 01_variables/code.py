247b11be91a1398d84c0f73d1caee9105631bf38
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-05 11:37:40 -0300

    01_variables

---
+++ 01_variables/code.py
@@ -0,0 +1,30 @@
+# python reads variable assignment from right to left
+
+x = 15 # int
+price = 9.99 # float
+discount = 0.2 # float
+
+result = price * (1 - discount)
+
+# this is a function that prints a variable on console
+# functions are things that perform actions or calculate outputs (or both)
+print(result)
+
+# strings
+
+name = "Rolf"
+
+print(name)
+
+# changing variable values
+
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17 # doesn't change a's value
+
+print(a)
+print(b)
