663030a04467e9af259f65cba8b6a8e66cebd01f
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-14 00:06:36 -0300

    30_relative_imports

---
+++ 30_relative_imports/code.py
@@ -0,0 +1,7 @@
+# this looks for a 'operations' package to import operator
+# from .operations import operator
+
+# this looks for a 'mylib' package in one level before this file
+# from ..mylib import *
+
+# hint: in 99% you will not use this :P
